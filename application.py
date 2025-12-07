51eb63716e904e9779275002c69b6978d3578771
Vijay Kumar <collegedays17@gmail.com>
2025-12-08 03:22:55 +0530

    This is for deployment

---
+++ application.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route("/")
+def home():
+    return "Hello from Flask on Elastic Beanstalk!"
+
+if __name__ == "__main__":
+    application.run()
